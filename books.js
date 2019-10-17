d788f2ccb0cfc7091ff7274a1d79914aaec31da7
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2019-10-17 12:22:04 +0530

    import schema in books

--- books.js
+++ books.js
@@ -1,9 +1,10 @@
+const { BookModel } = require("./schema/bookSchema");
+
 const book_resolver = function(req, res) {
-  var book_details = [
-    { name: "js", author: "max" },
-    { name: "react", author: "john snow" }
-  ];
-  res.send(book_details);
+  BookModel.find({}, function(err, docs) {
+    if (err) return err;
+    res.send(docs);
+  });
 };
 
 module.exports.book_resolver = book_resolver;
