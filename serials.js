42bcf7f124f6eb5573041bed81a388f8c06a5dc6
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2019-10-21 11:26:15 +0530

    remove array in serials and res only single object

--- serials.js
+++ serials.js
@@ -3,7 +3,7 @@ const { SerialModel } = require("./schema/serialsSchema");
 const serials_resolver = function(req, res) {
   SerialModel.find({}, function(err, docs) {
     if (err) return err;
-    res.send(docs);
+    res.send(docs[0]);
   });
 };
 module.exports.serials_resolver = serials_resolver;
