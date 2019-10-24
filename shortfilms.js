3d749dccdc0596d5f1fca84bfadbc321b3ed35e6
SRILAKSHMI576 <srilakshmigulla576@gmail.com>
2019-10-24 12:06:11 +0530

    Get all books by single id from db

--- shortfilms.js
+++ shortfilms.js
@@ -8,4 +8,13 @@ const shortfilms_Resolver = function(req, res) {
   });
 };
 
+const getShortfilmById = function(req, res) {
+  const shortfilm_id = req.params.shortfilmId;
+  const searchQuery = { _id: shortfilm_id };
+  shortfilmsModel.find(searchQuery, function(err, docs) {
+    if (err) return err;
+    res.send(docs);
+  });
+};
 module.exports.shortfilms_Resolver = shortfilms_Resolver;
+module.exports.getShortfilmById = getShortfilmById;
